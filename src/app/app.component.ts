eb27d94c24f5d6e820d5d70500f29376481e19e0
Carlos Daniel Barros <barros.carlosds@gmail.com>
2020-03-18 02:47:08 -0300

    add cadastro, remoção de pessoas e totalizador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,49 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
+
+import { Person } from './../models/person.model';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
+  templateUrl: './app.componet.html',
   styles: []
 })
 export class AppComponent {
-  title = 'cadastroPessoaTotalizador';
+  public title: string = 'Cadastro Simplificado de Pessoas';
+  public people: Array<Person> = [];
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(70),
+      ])]
+    });
+
+    this.people.push(
+      {name: 'Meliodas da Ira'},
+      {name: 'Merlin da Gula'},
+      {name: 'Ban da Ganância'},
+    )
+  }
+
+  addPerson() {
+    // const name = this.form.controls['name'].value;
+    const { name } = this.form.value;
+    const person = new Person(name);
+    this.people.push(person);
+
+    this.formClean();
+  }
+
+  removePerson(person) {
+    const index = this.people.indexOf(person);
+    this.people.splice(index, 1);
+  }
+
+  formClean() {
+    this.form.reset();
+  }
 }
