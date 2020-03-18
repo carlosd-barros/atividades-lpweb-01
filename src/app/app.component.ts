5ebf709282b9b299fb914ff6d78901605df1bebf
Carlos Daniel Barros <barros.carlosds@gmail.com>
2020-03-18 04:48:44 -0300

    add cadastro, remoção de pessoas, totalizador e estatísticas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,68 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
+
+import { Person } from './../models/person.model';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
+  templateUrl: './app.componet.html',
   styles: []
 })
 export class AppComponent {
-  title = 'cadastroPessoaEstatisticas';
+  public title: string = 'Cadastro Simplificado de Pessoas';
+  public statisticsHidden: boolean = true;
+  public people: Array<Person> = [];
+  public youngerPerson: Person;
+  public oldPerson: Person;
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(70),
+      ])],
+
+      age: ['', Validators.required],
+    });
+
+    this.people.push(
+      {name: 'Meliodas da Ira', age: 3000},
+      {name: 'Merlin da Gula', age: 1500},
+      {name: 'Ban da Ganância', age: 200},
+    );
+
+    this.setYoungerAndOldPerson();
+  }
+
+  addPerson() {
+    // const name = this.form.controls['name'].value;
+    const { name, age=0 } = this.form.value;
+    const person = new Person(name, age);
+    this.people.push(person);
+
+    this.formClean();
+    this.setYoungerAndOldPerson();
+  }
+
+  removePerson(person: Person) {
+    const index = this.people.indexOf(person);
+    this.people.splice(index, 1);
+    this.setYoungerAndOldPerson();
+  }
+
+  setYoungerAndOldPerson() {
+    const ages = this.people.map( ({age}) => age );
+
+    const maxAge = Math.max(...ages);
+    const minAge = Math.min(...ages);
+
+    this.youngerPerson = this.people.find(({ age }) => age === maxAge);
+    this.oldPerson = this.people.find(({ age }) => age === minAge);
+  }
+
+  formClean() {
+    this.form.reset();
+  }
 }
